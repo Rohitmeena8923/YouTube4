7ebb4819ffe603c7d6679bb8703895af9abf7c57
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-05-15 21:32:48 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,5 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "your_api_id"))
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+API_ID = int(os.getenv("API_ID", "27775431"))
+API_HASH = os.getenv("API_HASH", "b70bb1d45a1d05236671d4cc615e40f9")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8178852587:AAHJZKUTfmE-m78xrlmH9VvD9RUw1VXEU00")
